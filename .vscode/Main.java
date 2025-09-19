71fa921876531c7a1ef2dc8ffc7de6893221f94a
adrian-s06 <adrian.sukamto@gmail.com>
2025-09-19 18:15:07 +1000

    Password Generator (incomplete), basic printInfo() methods

---
+++ .vscode/Main.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main{
+    
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main (String[] args) { 
+        Scanner scnr = new Scanner(System.in);
+        Random rand = new Random();
+        
+        /*
+         * password must include the following:
+         * Capital
+         * lower case
+         * number
+         * symbol
+        */
+
+        //getting password length
+            System.out.println("Please enter the number of characters for your password (must be at least 4 characters):");
+        
+            int passLength = scnr.nextInt();
+            while (passLength < 4) {
+                System.out.println("------------------------------------------------------");
+                System.out.println("Invalid number of characters, please enter a number over 4");
+                passLength = scnr.nextInt();
+            }
+
+        //password generator
+            char[] password = new char[passLength];
+            
+            password[0] = (char) (rand.nextInt(26) + 64); // capital
+            password[1] = (char) (rand.nextInt(26) + 96); // lower case
+            password[2] = (char) rand.nextInt(9); // number
+            password[3] = (char) (rand.nextInt()); // symbol
+
+            for (int i = 4; i < passLength; i++) {
+                password[i] = (char) (rand.nextInt(94) + 32);
+            }
+
+        //password randomiser
+        
+        
+        // prepping website instance
+            String url = scnr.nextLine();
+            String userName = scnr.nextLine();
+            String finPassword = "Test";
+
+            Website website = new Website();
+        
+            website.setUrl(url);
+            website.setUsername(userName);
+            website.setPassword(finPassword);
+        
+            website.viewInfo();
+        
+        scnr.close();
+    }
+}
---
+++ .vscode/Website.java
@@ -0,0 +1,45 @@
+public class Website {
+      
+    private String webUrl, webUsername, webPassword;
+   
+    //constructor
+        public Website() {
+            webUrl = null;
+            webUsername = null;
+            webPassword = null;
+        }
+   
+    // set methods
+        public void setUrl(String url) {
+            webUrl = url;
+        }
+       
+        public void setUsername(String userName) {
+            webUsername = userName;
+        }
+       
+        public void setPassword(String password) {
+            webPassword = password;
+        }
+   
+    //get methods
+        public String getUrl() {
+            return webUrl;
+        }
+       
+        public String getUsername() {
+            return webUsername;
+        }  
+       
+        public String getPassword() {
+            return webPassword;
+        }
+   
+    public void viewInfo() {
+        System.out.println("Here are the following details:");
+        System.out.printf("Url: %s\nUsername: %s\nPassword: %s ", webUrl, webUsername, webPassword);
+       
+    }
+   
+
+}
