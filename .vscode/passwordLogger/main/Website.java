93c74c337204c6a7dbaf9a296f5d989e0cc0d103
adrian-s06 <adrian.sukamto@gmail.com>
2025-09-27 19:33:16 +1000

    Completed saveInfo() function

--- .vscode/passwordLogger/main/Website.java
+++ .vscode/passwordLogger/main/Website.java
@@ -1,14 +1,20 @@
 package passwordLogger.main;
 import java.io.PrintWriter;
+import java.util.UUID;
 public class Website {
       
-    private String webUrl, webUsername, webPassword;
+    private String webUrl, webUsername, webPassword, guid;
+    private long currTime;
    
     //constructor
         public Website() {
             webUrl = null;
             webUsername = null;
             webPassword = null;
+            
+            guid = "{" + UUID.randomUUID().toString() + "}";
+            currTime = System.currentTimeMillis();
+
         }
    
     // set methods
@@ -47,7 +53,7 @@ public class Website {
     public void saveInfo(PrintWriter outFS) {
         //format: "url","username","password","httpRealm","formActionOrigin","guid","timeCreated","timeLastUsed","timePasswordChanged"
         outFS.printf("\"url\",\"username\",\"password\",\"httpRealm\",\"formActionOrigin\",\"guid\",\"timeCreated\",\"timeLastUsed\",\"timePasswordChanged\"\n");
-        outFS.printf("\"%s\",\"%s\",\"%s\",\"%s\",\"formActionOrigin\",\"guid\",\"timeCreated\",\"timeLastUsed\",\"timePasswordChanged\"\n",webUrl,webUsername,webPassword,webUrl);
+        outFS.printf("\"%s\",\"%s\",\"%s\",,\"%s\",\"%s\",\"%d\",\"%d\",\"%d\"\n", webUrl , webUsername , webPassword , webUrl, guid , currTime , currTime, currTime );
     }
 
 }
